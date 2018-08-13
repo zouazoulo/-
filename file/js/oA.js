f5bfb7e4316d4fcc420681ef65bb4574b372df43
zouazoulo <1837244680@qq.com>
2018-08-13 09:55:10 +0800

    农场游戏部分功能

--- file/js/oA.js
+++
@@ -1,6 +0,0 @@
-define(function  (require, exports, module) {
-	function oA () {
-		console.log(1)
-	}
-	exports.oA = oA;
-})
--- file/js/one.js
+++
@@ -1,6 +0,0 @@
-require.config({
-	baseUrl : 'js',
-	paths:{
-		'jquery':'jquery'
-	}
-});
